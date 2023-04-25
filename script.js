78a61c9d25ea42860cc1761011aa30f366604ce0
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-25 12:34:01 +0530

    add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//your code here
+         //your code here
           const images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg"];
           const imageContainer = document.getElementById("imageContainer");
           const h3 = document.getElementById("h");
