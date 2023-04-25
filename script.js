a7414aa7d314f84846c913e191f03415ba67e1d5
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-25 12:21:13 +0530

    add

--- script.js
+++ script.js
@@ -1,50 +1,75 @@
 //your code here
-// Get all the image elements
-const images = document.querySelectorAll('img');
-// Get the reset button element
-const resetBtn = document.getElementById('reset');
-// Get the verify button element
-const verifyBtn = document.getElementById('verify');
-// Get the result paragraph element
-const para = document.getElementById('para');
-// Create an array to store the clicked images
-let clickedImages = [];
-
-// Add click event listener to each image
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        // If the clicked image is already in the clickedImages array, return
-        if (clickedImages.includes(image)) return;
-        // Add the clicked image to the clickedImages array
-        clickedImages.push(image);
-        // Change the border color of the clicked image
-        image.style.border = '2px solid blue';
-        // If two images are clicked, show the verify button and disable further clicks on images
-        if (clickedImages.length === 2) {
-            verifyBtn.style.display = 'block';
-            images.forEach(img => img.style.pointerEvents = 'none');
-        }
-    });
-});
-
-// Add click event listener to the reset button
-resetBtn.addEventListener('click', () => {
-    // Reset the clickedImages array
-    clickedImages = [];
-    // Hide the verify button
-    verifyBtn.style.display = 'none';
-    // Reset the border color of all images and enable clicks on images
-    images.forEach(img => {
-        img.style.border = 'none';
-        img.style.pointerEvents = 'auto';
-    });
-});
-
-// Add click event listener to the verify button
-verifyBtn.addEventListener('click', () => {
-    // Check if the clicked images are identical
-    if (clickedImages[0].classList.value === clickedImages[1].classList.value) {
-        // Display the "You are a human. Congratulations!" message
-        para.innerHTML = 'You are a human. Congratulations!';
-    } else {
-        // Display the "We can't
+          const images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg"];
+          const imageContainer = document.getElementById("imageContainer");
+          const h3 = document.getElementById("h");
+          const resetBtn = document.getElementById("reset");
+          const verifyBtn = document.getElementById("verify");
+          const para = document.getElementById("para");
+          let selectedImages = [];
+          let isVerifying = false;
+      
+          // Shuffle array
+          function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              [array[i], array[j]] = [array[j], array[i]];
+            }
+          }
+      
+          // Render images
+          function renderImages() {
+            shuffleArray(images);
+            for (let i = 0; i < images.length; i++) {
+              const img = document.querySelector(`.img${i + 1}`);
+              img.src = images[i];
+            }
+          }
+      
+          // Reset game
+          function resetGame() {
+            selectedImages = [];
+            isVerifying = false;
+            resetBtn.style.display = "none";
+            verifyBtn.style.display = "none";
+            para.innerHTML = "";
+            renderImages();
+          }
+      
+          // Check for identical images
+          function checkIdenticalImages() {
+            if (selectedImages.length === 2) {
+              if (selectedImages[0].src === selectedImages[1].src) {
+                para.innerHTML = "You are a human. Congratulations!";
+              } else {
+                para.innerHTML = "We can't verify you as a human. You selected the non-identical tiles.";
+              }
+              verifyBtn.style.display = "none";
+              isVerifying = true;
+            }
+          }
+      
+          // Event listener for image clicks
+          imageContainer.addEventListener("click", function(event) {
+            const img = event.target;
+            if (img.tagName === "IMG" && !isVerifying) {
+              if (!selectedImages.includes(img)) {
+                selectedImages.push(img);
+                img.style.border = "2px solid red";
+              }
+              checkIdenticalImages();
+            }
+          });
+      
+          // Event listener for reset button
+          resetBtn.addEventListener("click", function() {
+            resetGame();
+          });
+      
+          // Event listener for verify button
+          verifyBtn.addEventListener("click", function() {
+            checkIdenticalImages();
+          });
+      
+          // Initial render
+          renderImages();
+      
